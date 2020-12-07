fd002b9a9066cfbca8997f424aa6a086f5189a47
aszeto <aidenszeto@g.ucla.edu>
2020-12-07 12:05:41 -0800

    updated contact route

--- routes/contact.js
+++ routes/contact.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const router = express.Router();
-
+const { body, validationResult } = require('express-validator');
 const nodemailer = require('nodemailer');
 
 router.post(
   '/',
+  body('email').isEmail(),
   async (req,res) => {
     try {
-      const { name, email, text } = req.body
+      const { name, email, subject, text } = req.body
       // Create nodemailer transport to send reservation confirmation emails
       const transporter = nodemailer.createTransport({
         service: 'gmail',
@@ -16,24 +17,28 @@ router.post(
           pass: 'yowguokryuzjmbhj'
         }
       })
+      const ccto = [email, 'aiden@vhomesgroup.com']
       const userMailOptions = {
-        from: `"${name}" <reservations@vhomesgroup.com>`,
-        to: "vhomesgroup@gmail.com",
-        subject: `New message from ${name}`,
-        text: `From: ${email}
+        from: `"${name}" <vhomesgroup@gmail.com>`,
+        to: "contact@vhomesgroup.com",
+        cc: ccto,
+        subject: subject,
+        text:
+        `From: ${name}
+         Email: ${email}
 
-              ${text}`
+         ${text}`
       }
       transporter.sendMail(userMailOptions, (error, info) => {
         if (error) {
           console.log(error)
         }
         else {
-          console.log(`Message sent to ${email}`)
+          console.log(`Message sent to contact@vhomesgroup.com from ${email}`)
         }
       })
       res.status(200).json({
-          "message": `Email sent to ${email}`
+          "message": `Email sent to contact@vhomesgroup.com from ${email}`
       });
     }
     catch (error) {
