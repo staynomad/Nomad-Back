0b0d38b689d0e008c0f3176302ae0cf0396adefb
Seamus Le <seamust.le@gmail.com>
2020-12-18 22:42:32 -0800

    added error arg to catch block in reviews.js -> updated 200 status to 201 to show creation success

--- routes/reviews.js
+++ routes/reviews.js
@@ -9,7 +9,7 @@ const { requireUserAuth } = require("../utils");
 router.post(
   '/:listingId',
   requireUserAuth,
-  async(req, res) => {
+  async (req, res) => {
     try {
       // Rating is an integer ranging from 1-5
       // Review is the message posted by the user
@@ -28,24 +28,24 @@ router.post(
       for (let i = 0; i < reviewUsers.length; i++) {
         if (String(req.user._id) === String(reviewUsers[i])) {
           return res.status(400).json({
-              "errors": "You have already reviewed this listing."
+            "errors": "You have already reviewed this listing."
           })
         }
       }
       const listing = await Listing.findOneAndUpdate({ _id: listingId }, { $set: { rating: ratingData } }, { new: true })
       if (!listing) {
         return res.status(400).json({
-            "errors": "Listing does not exist. Please try again."
+          "errors": "Listing does not exist. Please try again."
         })
       }
-      res.status(200).json({
-          "message": "Review submitted successfully."
+      res.status(201).json({
+        "message": "Review submitted successfully."
       });
     }
-    catch {
+    catch (error) {
       console.log(error);
       res.status(500).json({
-          "errors": ["Error submitting review. Please try again!"]
+        "errors": ["Error submitting review. Please try again!"]
       });
     }
   }
