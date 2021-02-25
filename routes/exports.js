8c32aa79c8fb0c5d55b21335c8a895599c990793
Aiden Szeto <aszeto35@gmail.com>
2021-02-25 12:12:32 -0800

    added get route for ical file download

---
+++ routes/exports.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router()
+const { exportURL } = require('../config/index');
+
+router.get('/:filename', async (req, res) => {
+  const file = `./exports/${req.params.filename}`;
+  res.download(file);
+})
+
+module.exports = router;
