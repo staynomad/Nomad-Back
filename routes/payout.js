d4ae27a7183d6005197ab56416bd4c5e3ab33c3b
Nishant <nishantb1130@gmail.com>
2021-03-31 00:05:59 -0700

    onboarding payouts start

---
+++ routes/payout.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const router = express.Router();
+
+const { baseURL } = require('../config/index');
+
+const stripeSecretKey = process.env.STRIPE_SECRET_KEY
+const stripe = require('stripe')(stripeSecretKey);
+
+router.post('/payout', async(req, res) => {
+
+
+
+
+    const account = await stripe.accounts.create({
+        type: 'express',
+        country: 'US',
+        email: 'jenny.rosen@example.com',
+      });
+
+
+
+    const accountLinks = await stripe.accountLinks.create({
+    account: account.id,
+    refresh_url: `${baseURL}/payout`,
+    return_url: `${baseURL}`,
+    type: 'account_onboarding',
+    });
+});
+
+
+
+
