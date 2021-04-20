b6ae1ffad1b2c9e116b5042587e9124295b4eb24
aidenszeto <aszeto35@gmail.com>
2021-04-20 09:43:19 -0700

    delete container route

--- routes/container.js
+++ routes/container.js
@@ -3,9 +3,15 @@ const router = express.Router()
 
 const Container = require('../models/container.model');
 
+/*
+INPUT:
+  title (body) - string name of new container
+  listings (body) - array of listing IDs pertaining to new container
+*/
 router.post('/',
   async (req, res) => {
     try {
+      const { title, listings } = req.body
       const existingContainer = await Container.find({
         title: title,
       })
@@ -14,7 +20,6 @@ router.post('/',
           error: `A container called '${title}' already exists.`
         })
       }
-      const { title, listings } = req.body
       const container = await new Container({
         title: title,
         listings: listings
@@ -26,8 +31,34 @@ router.post('/',
     catch(error) {
       console.error(error);
       res.status(500).json({
-        "errors":
-        "Problem connecting with database. Please try again!"
+        error: "Problem connecting with database. Please try again!"
+      });
+    }
+})
+
+/*
+INPUT:
+  title (param) - string name of container to delete
+*/
+router.delete('/byTitle/:title',
+  async (req, res) => {
+    try {
+      const existingContainer = await Container.findOneAndRemove({
+        title: req.params.title,
+      }, (err) => {
+        return res.status(400).json({
+          error: "No containers exist with that title."
+        })
+        console.log(err)
+      })
+      return res.status(200).json({
+        message: `Successfully deleted ${req.params.title}`
+      })
+    }
+    catch(error) {
+      console.error(error);
+      res.status(500).json({
+        error: "Problem connecting with database. Please try again!"
       });
     }
 })
