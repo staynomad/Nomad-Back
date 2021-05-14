60528bcfbf22e2507b8261dcfee33788951de66d
Nishant <nishantb1130@gmail.com>
2021-05-13 17:05:46 -0700

    stripe onboarding

--- routes/payout.js
+++ routes/payout.js
@@ -6,32 +6,35 @@ const { baseURL } = require('../config/index');
 const stripeSecretKey = process.env.STRIPE_SECRET_KEY
 const stripe = require('stripe')(stripeSecretKey);
 
-router.post('/payout', async(req, res) => {
+router.post('/setup', async(req, res) => {
 
-  console.log("hi")
+  try{
+      const account = await stripe.accounts.create({
+          type: 'express',
+          country: 'US',
+        });
 
-  // TODO: Broken b/c connect api not connectd on stripe, go to https://stripe.com/docs/connect. 
-    const account = await stripe.accounts.create({
-        type: 'express',
-        country: 'US',
-        // email: 'jenny.rosen@example.com',
+      const accountLink = await stripe.accountLinks.create({
+      account: account.id,
+      refresh_url: `${baseURL}/myAccount`,
+      return_url: `${baseURL}/myAccount`,
+      type: 'account_onboarding',
       });
 
+      const redirectURL = accountLink['url'];
 
+      return res.status(200).json({
+        link: redirectURL,
+      })
+    } catch (e){
+      console.log(e)
+      return res.status(500).json({
+        'error': 'Stripe accountLink failed'
+      })
+    } 
+}
 
-    const accountLink = await stripe.accountLinks.create({
-    account: account.id,
-    refresh_url: `${baseURL}/myAccount`,
-    return_url: `${baseURL}`,
-    type: 'account_onboarding',
-    });
-
-    const redirectURL = accountLink['url'];
-
-    return res.status(200).json({
-      link: redirectURL,
-    })
-});
+);
 
 module.exports = router;
 
