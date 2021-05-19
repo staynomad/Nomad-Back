0dfd99ef139c8f601a79afaad5d0df57cbe68f58
Kenzie Tee <teekenzie@berkeley.edu>
2021-05-18 21:01:24 -0700

    added the missed added files

---
+++ routes/housekeeping.js
@@ -0,0 +1,17 @@
+const { Router } = require('express');
+const Housekeeping = require('../models/housekeeping.model');
+
+const router = Router();
+router.get('/:name', async (req, res) => {
+  try {
+    const { name } = req.params;
+    const result = await Housekeeping.findOne({ name });
+    res.status(200).json({ payload: result.payload });
+  } catch (err) {
+    res.status(500).json({
+      errors: 'There was an error getting the payload for ' + req.params.name,
+    });
+  }
+});
+
+module.exports = router;
