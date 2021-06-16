1ae8536b5fb64e822bd875a8e0eafaf30f181f5c
aidenszeto <aszeto35@gmail.com>
2021-06-16 16:07:53 -0700

    Fixed merge markers on user model

--- models/user.model.js
+++ models/user.model.js
@@ -1,41 +1,9 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const { Boolean, String } = Schema.Types;
+const { Array, Boolean, String } = Schema.Types;
 
 const UserSchema = new Schema({
-<<<<<<< HEAD
-  name: {
-    type: String,
-    required: [true, "Name cannot be blank"],
-  },
-  email: {
-    type: String,
-    index: { unique: true },
-    required: [true, "Email cannot be blank"],
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  check: {
-    type: String,
-  },
-  isHost: {
-    type: Boolean,
-  },
-  // Only required for hosts
-  isVerified: {
-    type: Boolean,
-  },
-  isPublic: {
-    type: Boolean,
-    default: false,
-  },
-  profileImg: {
-    type: String,
-  },
-=======
     name: {
         type: String,
         required: [true, 'Name cannot be blank'],
@@ -63,11 +31,13 @@ const UserSchema = new Schema({
         type: Array,
         default: []
     },
+    isVerified: {
+        type: Boolean,
+    },
     // Only required for hosts
     isVerified: {
         type: Boolean,
     },
->>>>>>> 1c9d41948eeaf42ce3c98ab3fce98307b926d99e
 });
 
 const User = mongoose.model("user", UserSchema);
