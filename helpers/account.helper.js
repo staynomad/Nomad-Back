75b60af977c60096d1be9a1c60bdd46c8e1fbe4b
Nishant Balaji <nishantb1130@gmail.com>
2021-07-20 15:56:27 -0700

    verification email

--- helpers/account.helper.js
+++ helpers/account.helper.js
@@ -1,5 +1,9 @@
 const Housekeeping = require("../models/housekeeping.model");
-const { sendEmail } = require("../helpers/nodemailer.helper");
+const {
+  sendEmail,
+  getHTML,
+  getAttachments,
+} = require("../helpers/nodemailer.helper");
 const { baseURL } = require("../config/index");
 
 // Increment users property in housekeeping collection
@@ -12,18 +16,25 @@ const incHousekeepingUsers = async () => {
   );
 };
 
-const sendVerificationEmail = async (email, userId) => {
+const sendVerificationEmail = async (name, email, userId) => {
+  const HTMLOptions = {
+    greeting: `Welcome, ${name}!`,
+    alert: "We're excited to have you!",
+    action: "Activate your account now",
+    description: `Click the button below or go to the following link to verify your acccount: 
+    <br /> <a style="text-decoration:none;color:white;" href="${baseURL}/accountVerification/${userId}">${baseURL}/accountVerification/${userId}</a>`,
+    buttonText: "Activate Account",
+    buttonURL: `${baseURL}/accountVerification/${userId}`,
+  };
+  const html = getHTML(HTMLOptions);
+  const attachments = getAttachments();
+
   const userMailOptions = {
     from: '"NomΛd" <reservations@visitnomad.com>',
     to: email,
-    subject: `NomΛd email verification needed`,
-    text: `Please click the following link to verify your account
-         ${baseURL}/accountVerification/${email}`,
-    html: `<p>
-          Please click
-          <a href="${baseURL}/accountVerification/${userId}">here</a>
-          to verify your account
-         </p>`,
+    subject: `Verify your NomΛd Account`,
+    html: html,
+    attachments: attachments,
   };
   sendEmail(userMailOptions);
 };
