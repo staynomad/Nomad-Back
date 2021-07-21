7fb2acc3d7333fd82986f38d94cb6166005281b7
Sydnee Tan <stanxy357@gmail.com>
2021-07-20 21:25:23 -0700

    host receives verification email if not verified

--- routes/user.js
+++ routes/user.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const User = require("../models/user.model");
 const { requireUserAuth } = require("../utils");
 const { multerUploads, uploadImagesToAWS } = require("./photos");
+const {sendVerificationEmail} = require('../helpers/account.helper')
 
 // Returns user object given a userId. If verbose = 1 is set, full friend objects are returned
 router.get("/getUserInfo/:userId", async (req, res) => {
@@ -223,6 +224,8 @@ router.post("/cancelFriendRequest", requireUserAuth, async (req, res) => {
       { _id: userId },
       { $pull: { outgoingFriendRequests: friendId } }
     );
+    user.friends.push = friendId;
+    // Add userId to friend's friends array
     const friend = await User.findOneAndUpdate(
       { _id: friendId },
       { $pull: { incomingFriendRequests: userId } }
@@ -278,7 +281,6 @@ router.post("/acceptFriendRequest", requireUserAuth, async (req, res) => {
       { _id: friendId },
       { $push: { friends: userId } }
     );
-
     if (!user || !friend) {
       return res.status(404).json({
         error: "Friend request not found. Please try again.",
