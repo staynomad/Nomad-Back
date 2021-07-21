27563f4db113f9e3537bce2cb0cc6bcb6defacc2
Nishant Balaji <nishantb1130@gmail.com>
2021-07-21 02:13:35 -0700

    email tester

--- helpers/emailtester.js
+++ helpers/emailtester.js
@@ -1,6 +1,10 @@
 const { sendReminder } = require("./expiredListings.helper.js");
 const { sendVerificationEmail } = require("./account.helper.js");
-const { sendConfirmationEmail } = require("./emails.helper.js");
+const {
+  sendConfirmationEmail,
+  sendTransferEmailConfirmation,
+  sendTransferInvite,
+} = require("./emails.helper.js");
 
 // Replace this with your own email address and name
 
@@ -8,9 +12,12 @@ const email = "nishantb1130@gmail.com";
 const name = "YOUR_NAME";
 const listing = "house";
 const listingID = "12345";
+const listings = ["12345", "54321"];
 const userId = "test";
 
 // Email functions to test
 // sendReminder(name, email, listing);
 // sendVerificationEmail(name, email, userId);
-sendConfirmationEmail(name, email, listingID);
+// sendConfirmationEmail(name, email, listingID);
+// sendTransferEmailConfirmation(name, email, "John", listings);
+// sendTransferInvite(name, email, "John");
