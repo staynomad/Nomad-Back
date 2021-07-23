1c5fb09d2ba0f470c1eb690c09e28b7c0bd794dc
Nishant Balaji <44332326+nishantbalaji@users.noreply.github.com>
2021-07-22 23:53:53 -0500

    Nodemailer Email Styling (#163)
    * email template
    
    * testing file
    
    * expiredListing email
    
    * html template
    
    * update html template
    
    * getHTML function call
    
    * update template
    
    * update raw html template file
    
    * add logo
    
    * attachment function
    
    * attachment function
    
    * verification email
    
    * tester file
    
    * tester file
    
    * text styling
    
    * generalize links
    
    * styling
    
    * change emailtester.js
    
    * update tester
    
    * confirmation email
    
    * transfer email
    
    * send transfer invite
    
    * email tester
    
    * email tester
    
    * send transfer email confirmation
    
    * precommit hook update
    
    * update sendReminder and create sendTransferRejection
    
    * host and guest reservation confirmations
    
    * checkout confirmations
    
    * checkin confirmations
    
    * cleaned up unused code
    
    * update styling and testers
    
    * styling
    
    * linting
    
    * styling updates
    
    * emailtester
    
    * update transfer accept and reject
    
    * linting
    
    * tester
    
    * edit styles
    
    * emailtester

--- helpers/account.helper.js
+++ helpers/account.helper.js
@@ -1,6 +1,4 @@
 const Housekeeping = require("../models/housekeeping.model");
-const { sendEmail } = require("../helpers/nodemailer.helper");
-const { baseURL } = require("../config/index");
 
 // Increment users property in housekeeping collection
 const incHousekeepingUsers = async () => {
@@ -12,23 +10,6 @@ const incHousekeepingUsers = async () => {
   );
 };
 
-const sendVerificationEmail = async (email, userId) => {
-  const userMailOptions = {
-    from: '"NomΛd" <reservations@visitnomad.com>',
-    to: email,
-    subject: `NomΛd email verification needed`,
-    text: `Please click the following link to verify your account
-         ${baseURL}/accountVerification/${email}`,
-    html: `<p>
-          Please click
-          <a href="${baseURL}/accountVerification/${userId}">here</a>
-          to verify your account
-         </p>`,
-  };
-  sendEmail(userMailOptions);
-};
-
 module.exports = {
   incHousekeepingUsers,
-  sendVerificationEmail,
 };
