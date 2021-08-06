e0b8c74bd3e23bad938982da081f3891594d9bde
Sydnee Tan <stanxy357@gmail.com>
2021-08-05 21:13:44 -0700

    Refactored login.js  and signup.js files to controllers

--- routes/login.js
+++ routes/login.js
@@ -1,52 +1,14 @@
 const express = require("express");
 const router = express.Router();
+const { check } = require("express-validator");
 
-const User = require("../models/user.model");
-const { getUserToken, validatePassword } = require("../utils");
-const { check, validationResult } = require("express-validator");
+const Login = require("../controllers/login.controller");
 
 /* User Login */
 router.post(
   "/",
   [check("email").isEmail().withMessage("The email address is not valid")],
-  async (req, res, next) => {
-    try {
-      const errors = validationResult(req);
-      if (!errors.isEmpty()) {
-        let invalidEmail = new Error({ name: "Invalid email format" });
-        invalidEmail.status = 401;
-        throw invalidEmail;
-      }
-      const { email, password } = req.body;
-      const user = await User.findOne({ email });
-      // explicit error handling for both user and password
-      // 404 - status code if the user does not exist
-      // 401 - status code if the passwords do not match
-      // 200 - if everything is ok
-      if (!user) {
-        let missingUser = new Error(
-          "The user with this email address does not exist"
-        );
-        missingUser.status = 404;
-        throw missingUser;
-      }
-
-      if (!validatePassword(password, user.password)) {
-        let incorrectPassword = new Error("Incorrect password.");
-        incorrectPassword.status = 401;
-        throw incorrectPassword;
-      }
-      const token = getUserToken(user);
-      return res.status(200).json({
-        token,
-        userId: user.id,
-        isHost: user.isHost,
-        user: user,
-      });
-    } catch (error) {
-      next(error);
-    }
-  }
+  Login.login
 );
 
 module.exports = router;
--- routes/signup.js
+++ routes/signup.js
@@ -1,12 +1,7 @@
 const express = require("express");
-const User = require("../models/user.model");
-const { getUserToken, passGenService } = require("../utils");
-const { check, body, validationResult } = require("express-validator");
 const router = express.Router();
-const {
-  incHousekeepingUsers,
-  sendVerificationEmail,
-} = require("../helpers/account.helper");
+const { check, body } = require("express-validator");
+const Signup = require("../controllers/signup.controller");
 
 /* POST users listing. */
 router.post(
@@ -33,60 +28,7 @@ router.post(
       .matches(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/)
       .withMessage("Password must contain one special character"),
   ],
-  async (req, res) => {
-    try {
-      // data validation
-      // why 422 status code? -> https://www.bennadel.com/blog/2434-http-status-codes-for-invalid-data-400-vs-422.htm
-      let errors = validationResult(req).array();
-      const { name, email, password, isHost } = req.body;
-      const user = await User.findOne({ email });
-      if (user) {
-        const emailError = {
-          value: email,
-          msg: `User already exists with email ${email}`,
-          param: "email",
-          location: "body",
-        };
-        errors.push(emailError);
-      }
-      if (errors.length !== 0) return res.status(422).json(errors);
-      // encrypt the password
-      const encrypted_password = await passGenService(password);
-      // create a new user
-      const data = {
-        name,
-        email,
-        password: encrypted_password,
-        isHost,
-        isPublic: isHost,
-      };
-      // Set isVerified to false only if user is a host
-      if (isHost) {
-        data.isVerified = false;
-      }
-      const newUser = await new User(data).save();
-      incHousekeepingUsers();
-      if (isHost) {
-        sendVerificationEmail(data.email, newUser._id);
-      }
-
-      // now send the token
-      const token = getUserToken({ id: newUser._id });
-
-      // we could send the 200 status code
-      // but 201 indicates the resource is created
-      res.status(201).json({
-        token,
-        userId: newUser._id,
-      });
-    } catch (error) {
-      // explicit error catching
-      console.error(error);
-      res.status(500).json({
-        errors: ["Error signing up user. Please try again!"],
-      });
-    }
-  }
+  Signup.signup
 );
 
 module.exports = router;
